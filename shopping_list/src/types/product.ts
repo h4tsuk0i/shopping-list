755684ec9f7e50ed581722f6441f29c7e99f2063
h4tsuk0i <lolligerkill@gmail.com>
2025-09-04 12:56:55 +0200

    created temp card component

---
+++ shopping_list/src/types/product.ts
@@ -0,0 +1,12 @@
+type Product = {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    discountPercentage: number;
+    rating: number;
+    stock: number;
+    tags: string[];
+    meta: string[];
+    images: string[];
+};
